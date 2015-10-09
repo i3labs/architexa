7c4aa00135cc3689943742282cb1d967009070f1
Vineet Sinha <vineet.sinha@gmail.com>
2015-10-08 20:27:17 -0400

    moving links to be relative to /architexa

---
+++ js/jquery.prettyPhoto-old.js
@@ -0,0 +1,8 @@
+/* ------------------------------------------------------------------------
+	Class: prettyPhoto
+	Use: Lightbox clone for jQuery
+	Author: Stephane Caron (http://www.no-margin-for-errors.com)
+	Version: 2.5.2
+------------------------------------------------------------------------- */
+
+
